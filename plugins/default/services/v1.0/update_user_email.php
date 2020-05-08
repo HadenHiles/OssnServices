27dfe9af178c64d6cbfd6351d4e4d07931504a1d
Haden <hadenhiles@gmail.com>
2020-05-08 14:42:45 -0400

    If user isn't logged in to the account they are trying to modify then send them to the OAuth login

--- plugins/default/services/v1.0/update_user_email.php
+++ plugins/default/services/v1.0/update_user_email.php
@@ -44,5 +44,11 @@
  				$user = $params['OssnServices']->setUser(ossn_user_by_guid($user->guid)); //get user again with new contents
  				$params['OssnServices']->successResponse($user);
  		}
+ } else {
+   $component = new OssnComponents;
+   $settings  = $component->getSettings('OAuthLogin');
+   if (!empty($settings)) {
+     header('Location: ' . $settings->wp_consumer_authorization_url . '?response_type=code&client_id=' . $settings->wp_consumer_key . '&redirect_uri=' . ossn_site_url('oauth_login/wordpress'));
+   }
  }
  $params['OssnServices']->throwError('103', ossn_print('ossnservices:usereditfailed'));
